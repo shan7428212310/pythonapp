c19778d2b2bf089e5a1281387fc93f71336a120d
shan7428212310 <121659363+shan7428212310@users.noreply.github.com>
2024-07-16 19:09:35 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+# Use an official Python runtime as a parent image
+FROM python:3.10.10-slim
+
+# Set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# Set the working directory in the container
+WORKDIR /app
+
+# Copy the requirements file for dependencies
+COPY requirements.txt /app/
+
+# Install any needed packages specified in requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the rest of the application code into the container
+COPY . /app/
+
+# Run the application
+CMD ["python", "TextSearch.py"]
